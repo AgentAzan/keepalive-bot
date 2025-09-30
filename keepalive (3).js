cba212c88b8cca4ee5907c481cd25d3bd40dabbf
AgentAzan <ccrazyloxlopa@gmail.com>
2025-09-30 14:51:01 +0200

    Add files via upload

---
+++ keepalive (3).js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is alive!');
+});
+
+function keepAlive() {
+  app.listen(PORT, () => {
+    console.log(`✅ Keepalive server is running on port ${PORT}`);
+  });
+}
+
+module.exports = keepAlive;
